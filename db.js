7008328e50ead55c740e0661b6e663320dcdc9a6
rod-yordan <i2414151@continental.edu.pe>
2025-09-21 14:38:55 -0500

    cambiado el host

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql2/promise');
 // Crear pool de conexiones para manejar múltiples requests
 const pool = mysql.createPool({
-host: 'mysql.railway.internal',
+host: 'shortline.proxy.rlwy.net',
 user: 'root', // tu usuario de MySQL
 password: 'hUOdcOJJOVQaRNyDpYvXupVjvvMuxtwj', // tu contraseña de MySQL
 database: 'railway'
